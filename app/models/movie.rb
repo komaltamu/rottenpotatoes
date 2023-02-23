d95107b7794f2efc1b98f5aa7ca92ed404aa43a5
komalilyas <komal.ilyas9@gmail.com>
2023-02-22 21:03:47 -0600

    unique rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
+  def self.unique_ratings
+    self.distinct.pluck(:rating)
+end
     def self.ratings
       Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
